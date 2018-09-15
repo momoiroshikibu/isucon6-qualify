5a2a58cfe732bf793bbec36ce6afe85ac483fb20
momoiroshikibu <momoiroshikibu@gmail.com>
2018-09-15 10:03:03 +0900

    bench

--- include/bench.mk
+++ include/bench.mk
@@ -1,2 +1,2 @@
 bench.execute:
-	@ssh $(SSH_USER)@$(SSH_HOST) 'cd /home/isucon/isubata/bench && ./bin/bench --remotes=127.0.0.1 -output result_$(DATETIME).json'
+	@ssh $(SSH_USER)@$(SSH_HOST) 'cd /home/isucon/isucon6q && ./isucon6q-bench http:127.0.0.1'
